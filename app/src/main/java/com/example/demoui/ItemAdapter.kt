9b5c763bb4ab7a1b02c24a362073748e7d39fc9c
JASPERINDIA\mohammad.shakir.in <mohammad.shakir.in@snapdeal.com>
2021-12-29 16:06:28 +0530

    REcyclerView added

--- app/src/main/java/com/example/demoui/ItemAdapter.kt
+++ app/src/main/java/com/example/demoui/ItemAdapter.kt
@@ -1,24 +1,40 @@
 package com.example.demoui
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ItemAdapter(): RecyclerView.Adapter<ItemViewHolder>() {
+class ItemAdapter(var items:List<Item>): RecyclerView.Adapter<ItemViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.shop_by_price_item, parent, false)
+        return ItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-
+        val item = items[position]
+        holder.bind(item)
     }
 
     override fun getItemCount(): Int {
-
+        return items.size
     }
 
 }
 
 class ItemViewHolder(view: View): RecyclerView.ViewHolder(view){
+    private lateinit var item:Item
 
+//    private val image:ImageView = itemView.findViewById(R.id.iv_item)
+    private val itemTitle: TextView = itemView.findViewById(R.id.tv_item_name)
+    private val itemSubtitle:TextView = itemView.findViewById(R.id.tv_item_price)
+
+    fun bind(item:Item){
+        this.item = item
+        itemTitle.text = this.item.tileTitle
+        itemSubtitle.text = this.item.tileSubTitle
+    }
 }
--- app/src/main/java/com/example/demoui/MainActivity.kt
+++ app/src/main/java/com/example/demoui/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.demoui.Utils.Companion.getJsonDataFromAsset
 import com.example.demoui.databinding.ActivityMainBinding
 import com.google.gson.Gson
@@ -25,5 +26,9 @@ class MainActivity : AppCompatActivity() {
 
         val widget:Widget = gson.fromJson(jsonFileString,Widget::class.java)
         Log.d(TAG, "widget: "+widget)
+        val items: List<Item> = widget.collections
+
+        binding.recyclerView.layoutManager = GridLayoutManager(this,2)
+        binding.recyclerView.adapter = ItemAdapter(items)
     }
 }
